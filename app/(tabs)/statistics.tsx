a967cd36b5aafc8851520cea7b557d4688a897bd
danil <d_davletov@internet.ru>
2025-08-07 23:30:22 +0200

    Добавлено навигационное меню

---
+++ app/(tabs)/statistics.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { YStack } from "tamagui"
+
+const Statistics = () => {
+    return (
+        <YStack></YStack>
+    )
+}
+
+export default React.memo(Statistics)
