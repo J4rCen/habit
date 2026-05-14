e50118ddb0f44d5211ea4738852c9b02da5ebfe9
danil <d_davletov@internet.ru>
2026-05-14 15:36:49 +0300

    Поправлена верстка при выборе времени суток

--- app/screens/interval_day/index.tsx
+++ app/screens/interval_day/index.tsx
@@ -55,39 +55,39 @@ const IntervalDay = () => {
 				<YStack height={SCREEN_HEIGHT} backgroundColor='$dark' alignItems="center">
 					<ScrollView maxHeight={SCREEN_HEIGHT} style={{ backgroundColor: '#222831' }} showsVerticalScrollIndicator={false}>
 						<YStack width={SCREEN_WIDTH - 20} gap={20} alignItems="center" paddingTop={10}>
-							<XStack gap={10}>
+							<XStack gap={10} width={'100%'} display="flex" justifyContent='space-between'>
 								<XStack alignItems="center" justifyContent='flex-start'>
 									<MorningSvg size={SCREEN_WIDTH_400 ? 28 : 32} />
 									<Text color={'white'} fontSize={SCREEN_WIDTH_400 ? 20 : 24}>{t('createHabit.morning')}</Text>
 								</XStack>
 								<CustomTimePicker
-									width={SCREEN_WIDTH * 0.7}
+									width={SCREEN_WIDTH * 0.6}
 									height={50}
 									value={morningTime}
 									onChange={setMorningTime}
 									placeholder={t('settings.morningStart')}
 								/>
 							</XStack>
-							<XStack gap={10}>
+							<XStack gap={10} width={'100%'} display="flex" justifyContent='space-between'>
 								<XStack alignItems="center" justifyContent='flex-start'>
 									<DaySvg size={SCREEN_WIDTH_400 ? 28 : 32} />
 									<Text color={'white'} fontSize={SCREEN_WIDTH_400 ? 20 : 24}>{t('createHabit.day')}</Text>
 								</XStack>
 								<CustomTimePicker
-									width={SCREEN_WIDTH * 0.7}
+									width={SCREEN_WIDTH * 0.6}
 									height={50}
 									value={dayTime}
 									onChange={setDayTime}
 									placeholder={t('settings.dayStart')}
 								/>
 							</XStack>
-							<XStack gap={10}>
+							<XStack gap={10} width={'100%'} display="flex" justifyContent='space-between'>
 								<XStack alignItems="center" justifyContent='flex-start'>
 									<EveningSvg size={SCREEN_WIDTH_400 ? 22 : 26} />
 									<Text color={'white'} fontSize={SCREEN_WIDTH_400 ? 20 : 24}>{t('createHabit.evening')}</Text>
 								</XStack>
 								<CustomTimePicker
-									width={SCREEN_WIDTH * 0.7}
+									width={SCREEN_WIDTH * 0.6}
 									height={50}
 									value={eveningTime}
 									onChange={setEveningTime}
